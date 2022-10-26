fd7059a9c1b686372873b566c93bce701b537b1d
Santiago Bernal <exactlimon@gmail.com>
2022-10-26 18:54:18 -0500

    [add] expressJS router demo

---
+++ clase08/ejercicio1.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const { Router } = express
+
+const app = express();
+
+// mascotas
+const routerPets = new Router();
+
+routerPets.use(express.json());
+
+const pets = [];
+
+routerPets.get('/', (req, res) => {
+  res.send(pets);
+});
+
+routerPets.post('/', (req, res) => {
+  const { body } = req;
+  pets.push(body);
+  res.send(body);
+});
+
+// personas
+
+const routerPeople = new Router();
+
+routerPeople.use(express.json());
+
+const people = [];
+
+
+routerPeople.get('/', (req, res) => {
+  res.send(people);
+});
+
+routerPeople.post('/', (req, res) => {
+  const { body } = req;
+  people.push(body);
+  res.send(body);
+});
+
+
+
+app.use('/mascotas', routerPets);
+app.use('/personas', routerPeople);
+
+const port = 8080;
+const server = app.listen(port, () => {
+  console.log(`Servidor escuchando en http://localhost:${port}`);
+});
+
+server.on('error', (err) =>
+  console.log('Error en el servidor:', err)
+);
