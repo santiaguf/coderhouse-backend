15b5140d875fa8f7c54b0a716bfb40043f1951ee
Santiago Bernal <exactlimon@gmail.com>
2022-11-14 19:18:10 -0500

    [add] demo typescript compiler

---
+++ clase13/demo-typescript/index.js
@@ -0,0 +1,3 @@
+var lista = [2, 3, 5, 6, 7];
+lista.map(function (x) { return x * x; })
+    .forEach(function (y) { return console.log(y); });
---
+++ clase13/demo-typescript/index.ts
@@ -0,0 +1,4 @@
+const lista:Array<number>=[2,3,5,6,7];
+
+lista.map((x:number) => x*x)
+.forEach((y:number) => console.log(y));
