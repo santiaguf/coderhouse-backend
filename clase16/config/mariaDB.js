5b554a0567a6436df4a9871d420dfe1c0eec1197
Santiago Bernal <exactlimon@gmail.com>
2022-11-23 20:16:52 -0500

    feat: mysql connection with env file

--- clase16/config/mariaDB.js
+++ clase16/config/mariaDB.js
@@ -1,9 +1,12 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 export const config = {
   client: 'mysql',
   connection: {
-    host: 'localhost',
-    user: '',
-    password: '',
-    database: 'ecommerce'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
   }
 }
