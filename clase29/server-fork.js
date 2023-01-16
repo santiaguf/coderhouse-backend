5efb934cb377d655b896a8a1eecaf8c820f388a5
Santiago Bernal <exactlimon@gmail.com>
2023-01-16 18:51:05 -0500

    feat: server fork demo

---
+++ clase29/server-fork.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = parseInt(process.argv[2]) || 8080;
+
+app.get('/', (req, res) => {
+  res.send(`Servidor express en ${PORT} - <b>pid ${process.pid}</b> - ${new Date().toLocaleString()}`);
+});
+
+const server = app.listen(PORT, () => {
+  console.log(`Servidor express escuchando en http://localhost:${PORT} - PID ${process.pid}}`);
+});
+server.on('error', error => console.log(`Error en servidor ${error}`));
