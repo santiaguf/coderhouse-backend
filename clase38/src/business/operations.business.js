2133b09875bb0f7498d16a5fc18c15afc6ab25f7
Santiago Bernal <exactlimon@gmail.com>
2023-02-15 19:12:47 -0500

    feat: business layer

---
+++ clase38/src/business/operations.business.js
@@ -0,0 +1,43 @@
+import { suma, resta, mult, div} from 'sb-calculadora';
+import { save, getAll as getAllOps } from '../persistence/operations.persistence.js';
+
+async function sum(a, b) {
+    const resultOp = suma(a, b);
+
+    await save({ params: [a, b] , result: resultOp, operation: 'suma', tiemstamp: Date.now() });
+    return result;
+}
+
+async function substract(a, b) {
+    const resultOp = resta(a, b);
+
+    await save({ params: [a, b] , result: resultOp, operation: 'resta', tiemstamp: Date.now() });
+    return result;
+}
+
+async function multiply(a, b) {
+    const resultOp = mult(a, b);
+
+    await save({ params: [a, b] , result: resultOp, operation: 'multiplicacion', tiemstamp: Date.now() });
+    return result;
+}
+
+async function divide(a, b) {
+    const resultOp = div(a, b);
+
+    await save({ params: [a, b] , result: resultOp, operation: 'division', tiemstamp: Date.now() });
+    return result;
+}
+
+async function getAll() {
+    return await getAllOps();
+}
+
+export {
+    sum,
+    substract,
+    multiply,
+    divide,
+    getAll
+};
+
