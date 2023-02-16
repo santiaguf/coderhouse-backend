b1cbccb7bacdd7449a4c92e92a833c496c17748f
Santiago Bernal <exactlimon@gmail.com>
2023-02-15 19:24:37 -0500

    feat: server layer

---
+++ clase38/src/server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import operationsRoutes from "./routes/operations.routes.js";
+
+const app = express();
+
+
+app.get("/", (req, res) => {
+    res.send("Hola mundo!");
+});
+
+app.use("/api/v1/operations", operationsRoutes);
+
+const PORT = process.env.PORT || 8080;
+
+const server = app.listen(PORT, () => {
+    console.log(`Servidor express escuchando en http://localhost:${PORT} - PID ${process.pid}`);
+});
+server.on('error', error => console.log(`Error en servidor ${error}`));
