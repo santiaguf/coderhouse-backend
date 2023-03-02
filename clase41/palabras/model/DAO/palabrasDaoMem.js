88db48e93634e7664e55d6694ed1595c036f930c
Santiago Bernal <exactlimon@gmail.com>
2023-03-01 19:07:16 -0500

    feat: palabras dao memory

---
+++ clase41/palabras/model/DAO/palabrasDaoMem.js
@@ -0,0 +1,37 @@
+import palabraDTO from "../DTO/palabraDto.js";
+import PalabrasBaseDAO from "./palabrasBaseDao.js";
+
+class PalabrasDAOMem extends PalabrasBaseDAO {
+    constructor() {
+        super();
+        this.palabras = [];
+    }
+
+    obtenerPalabras = async () => {
+        try {
+            return this.palabras;
+        }
+        catch (error) {
+            console.log(`Error en obtenerPalabras  ${error}`);
+            return [];
+        }
+    }
+
+    guardarPalabra = async palabra => {
+        try {
+            let _id = this.getNext_Id(this.palabras);
+            let timestamp = new Date.now();
+            let palabraGuardada = palabraDTO(palabra, _id, timestamp);
+            this.palabras.push(palabraGuardada);
+
+            return palabraGuardada;
+
+        } catch (error) {
+            console.log(`Error en guardarPalabra  ${error}`);
+            let palabra = {};
+            return palabra;
+        }
+    }
+}
+
+export default PalabrasDAOMem;
