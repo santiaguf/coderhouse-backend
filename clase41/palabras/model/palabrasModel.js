7220936d71e6627418c384a384ac01fba78fa106
Santiago Bernal <exactlimon@gmail.com>
2023-03-01 19:19:52 -0500

    feat: dependencies and palabra model

---
+++ clase41/palabras/model/palabrasModel.js
@@ -0,0 +1,22 @@
+import Joi from 'joi';
+
+class Palabras {
+    constructor(palabra) {
+        this.palabra = palabra;
+    }
+
+    equals(otroPalabra) {
+        if(!(otroPalabra instanceof Palabras)) return false;
+        if( this.palabra != otroPalabra.palabra) return false;
+        return true;
+    }
+
+    static validar(palabra) {
+        const palabraSchema = Joi.object({
+            palabra:  requerido? Joi.string().required() : Joi.string()
+        })
+
+        const { error } = palabraSchema.validate(palabra);
+        if(error) throw new Error(error.details[0].message);
+    }
+}
