48e83ff826b5aae2aa2eef7f7f005319e9573f47
Santiago Bernal <exactlimon@gmail.com>
2023-03-06 20:17:31 -0500

    feat: basic unit test

---
+++ clase42/randomNumbers/serverNumeros.test.js
@@ -0,0 +1,44 @@
+import { strictEqual, deepStrictEqual } from 'assert';
+
+import axios from 'axios';
+
+const sendNumber = number => axios.post('http://localhost:8080/ingreso', { number });
+const getNumbers = () => axios('http://localhost:8080/egreso');
+
+describe('comprobando que el servidor fuincione correctamente', function () {
+    
+    /*
+    before(function() {
+        console.log('*** comienzo del test ***')
+    })
+
+    after(function() {
+        console.log('*** fin del test ***')
+    })
+
+    beforeEach(function() {
+        console.log('*** comienzo de una prueba ***')
+    })
+
+    afterEach(function() {
+        console.log('*** fin de una prueba ***')
+    }) */
+
+    it('deberia devolver un array vacio', async function () {
+        const { data } = await getNumbers();
+        strictEqual(data.numbers.length, 0);
+    });
+
+
+
+    it('deberia guardar y luego recibir 10 números consecutivos', async function () {
+        for (let i = 0; i < 10; i++) {
+            await sendNumber(i);
+        }
+
+        const { data } = await getNumbers();
+
+        strictEqual(data.numbers.length, 10);
+        deepStrictEqual(data.numbers, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+    })
+})
